cc1db8aa98c400b999c2eed677cb355a746547f6
julialanapatto <julialanapatto@gmail.com>
2022-07-20 17:06:00 -0300

    fix:code error

--- src/middlewares/accountMiddleware.js
+++ src/middlewares/accountMiddleware.js
@@ -3,9 +3,9 @@ const accountService = require('../services/accountService');
 const validateDeposit = async (req, res, next) => {
   const { codCliente, valor } = req.body;
 
-  if(!codCliente) res.status(403).json({ message: 'Código do cliente é obrigatório' })
+  if(!codCliente) res.status(400).json({ message: 'Código do cliente é obrigatório' })
 
-  if(!valor) res.status(403).json({ message: 'Valor é obrigatório' })
+  if(!valor) res.status(400).json({ message: 'Valor é obrigatório' })
 
   if (valor <= 0) res.status(403).json({ message: 'A quantidade a ser depositada é inválida' })
 
@@ -16,9 +16,9 @@ const validateWithdraw = async (req, res, next) => {
 
   const { codCliente, valor } = req.body; 
 
-  if(!codCliente) res.status(403).json({ message: 'Código do cliente é obrigatório' })
+  if(!codCliente) res.status(400).json({ message: 'Código do cliente é obrigatório' })
 
-  if(!valor) res.status(403).json({ message: 'Valor é obrigatório'})
+  if(!valor) res.status(400).json({ message: 'Valor é obrigatório'})
 
   const balance = await accountService.getByAccount(codCliente);
   
