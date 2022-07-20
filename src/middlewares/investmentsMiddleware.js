49848ff8efd67de45f95dc621ac7f4cfcbbdf8a6
julialanapatto <julialanapatto@gmail.com>
2022-07-20 16:38:37 -0300

    feat: validate empty fields

--- src/middlewares/investmentsMiddleware.js
+++ src/middlewares/investmentsMiddleware.js
@@ -2,11 +2,17 @@
 const investmentsService = require('../services/investmentsService');
 
 const validatePurchase = async (req, res, next) => {
-  const { codAtivo, qtdeAtivo } = req.body; 
+  const {codCliente, codAtivo, qtdeAtivo } = req.body; 
+
+  if (!codCliente) return res.status(400).json({ message: 'código do cliente is required' });
+
+  if(!codAtivo)  return res.status(400).json({ message: 'código do ativo is required' });
+
+  if(!qtdeAtivo) return res.status(400).json({ message: 'quantidade do ativo is required' });
   
   const availableAssets = await investmentsService.getByAsset(codAtivo);
   
-   if (qtdeAtivo > availableAssets.qtdeAtivo) return res.status(403).json({ message: 'Quantidade de ativo a ser comprada não pode ser maior que a quantidade disponível na corretora' });
+  if (qtdeAtivo > availableAssets.qtdeAtivo) return res.status(403).json({ message: 'Quantidade de ativo a ser comprada não pode ser maior que a quantidade disponível na corretora' });
 
   next();
 }
